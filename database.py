4efe5064f3e554bb9da77321518a6307a52e7d51
Kirill Shklyarenko <shklyara94@gmail.com>
2019-10-08 18:44:59 +0300

    for homework

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import psycopg2
 
 with psycopg2.connect(dbname='telemetry', user='postgres',
                       password='123', host='localhost') as conn:
-    with conn.cursor as cur:
+    with conn.cursor() as cur:
         cur.execute("SELECT * FROM air_track_periods")
         res = cur.fetchall()
         print(res)
