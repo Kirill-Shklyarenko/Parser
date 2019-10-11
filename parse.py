cc7d357fc9e379323712e9b7e042444c5c64cf7e
Kirill Shklyarenko <shklyara94@gmail.com>
2019-10-11 15:47:56 +0300

    All WORKS GREAT! +_-

--- parse.py
+++ parse.py
@@ -1,21 +1,10 @@
 from struct import *
 import psycopg2
-from database import *
+
 
 
 planner = r'Planner'
 planner_RSF = r'Planner.rsf'
-"""
-def access_bit(data, num):
-    base = int(num // 8)
-    shift = int(num % 8)
-    return (data[base] & (1 << shift)) >> shift
-
-    qwe = [access_bit(word, i) for i in range(len(word) * 8)]
-    res = functools.reduce(lambda total, d: 10 * total + d, qwe, 0)
-    asd = bin(word)
-    byte_shift = bin(res) >> 2
-"""
 
 
 def line_parser(number_of_line):
@@ -29,76 +18,123 @@ def value_of_lines():
         lines = file.readlines()
         return int(len(lines))
 
-def byte_reader(type_w, size, wib):
+def byte_reader(type_w, size, offset):
     with open(planner_RSF, 'rb') as file:
         header = file.read(14).decode('cp1252')  # Имя файла заголовка [0 - 14]
-        wib *= 2  # Номер слова в блоке умножаем на размер слова (2 байта)
-        file.seek(wib + 14)  # Прибавляем 14 байт, чтобы отсечь Имя файла заголовка
+        offset *= 2  # Номер слова в блоке умножаем на размер слова (2 байта)
+        file.seek(offset + 14)  # Прибавляем 14 байт, чтобы отсечь Имя файла заголовка
         word = file.read(size)
         value = unpack(type_w, word)[0]
-        print('BYTES: ' + str(word))
-        print('VALUE: ' + str(value))
+        # print('BYTES: ' + str(word))
+        # print('VALUE: ' + str(value))
         return value
 
-def parse_planner_rsf():
+def parse_planner_stn():
     number_of_line = 3
     vol = value_of_lines()
-    info = ['group_name', ['variable_name', 'type', 'offset']]
+    struct = []
     while number_of_line < vol:
         line = line_parser(number_of_line)
-
         # Поиск имени группы
-        # Если строка начинается с ';' для случаев где есть "(MAPKI -> MTO) и т.д."
+        # Если строка начинается с ';'
         if ';' in line[0]:
             line.remove(';')
             nwline = ''.join(line)
             # Проверка следующей строки (не является ли она именем группы)
             next_line = line_parser(number_of_line + 1)
             if ';' in next_line[0]:
-                info.append(next_line)
+                struct.append(next_line)
                 number_of_line += 1
             else:
-                info.append(nwline)
+                struct.append(nwline)
                 number_of_line += 1
 
-        # Поиск параметров
+        # Поиск описания переменных
         elif len(line) > 3:
             # Определяем тип переменной
             if 'W' in line[5]:
-                type_w = 'I'  # UINT_2t
-                size = 2
+                # type_w = 'I'  # UINT_2t
+                # size = 2
+                ca = [line[0], line[5], int(line[1])]
+                struct.append(ca)
+                number_of_line += 1
             if 'S' in line[5]:
-                type_w = 'i'  # INT_2t
-                size = 2
+                # type_w = 'i'  # INT_2t
+                # size = 2
+                ca = [line[0], line[5], int(line[1])]
+                struct.append(ca)
+                number_of_line += 1
             if 'U' in line[5]:
+                next_line = line_parser(number_of_line + 1)
+                prev_offset = int(line[1])
+                curr_offset = int(next_line[0])
+                ca = [line[0], line[5], [curr_offset, prev_offset]]
+                struct.append(ca)
+                number_of_line += 2
+            if 'L' in line[5]:
+                # type_w = ''
+                # size = 4
+                next_line = line_parser(number_of_line + 1)
+                prev_offset = int(line[1])
+                curr_offset = int(next_line[0])
+                ca = [line[0], line[5], [curr_offset, prev_offset]]
+                struct.append(ca)
+                number_of_line += 2
+            if 'R' in line[5]:
+                # type_w = ''
+                # size = 1
+                ca = [line[0], line[5], int(line[1])]
+                struct.append(ca)
+                number_of_line += 1
+            if 'F' in line[5]:
+                ca = [line[0], line[5], int(line[1])]
+                struct.append(ca)
+                number_of_line += 1
+    return struct
+
+def parse_planner_rsf(struct):
+    for string in struct:
+        if len(string) == 3:
+            if 'W' in string[1]:
+                lba = asd
+            elif 'S' in string[1]:
+                asd = asdd
+            elif 'UU' in string[1]:
                 type_w = '<i'
                 size = 4  # Размер 4 байта потому что используется 2 слова х 2 байта идущие друг за другом
-                next_line = line_parser(number_of_line + 1)
+                #for v in string[2]:
+                offset = string[2][0]
+                name = string[0]
+                value = byte_reader(type_w, size, offset)
+                string.clear()
+                string.insert(0, name)
+                string.insert(1, value)
 
-                prev_wib = info['word_in_block']
-                curr_wib = int(next_line[0])
-
-                info['word_in_block'] = [curr_wib, prev_wib]
-                info['mask'] = next_line[1]
-                info['shift'] = int(next_line[2])
-                info['value'] = byte_reader(type_w, size, curr_wib)
-                number_of_line += 2
-                print(info, '\r\n')
-            if 'L' in line[5]:
-                type_w = ''
-                size = 4
-            if 'R' in line[5]:
-                type_w = ''
-                size = 1
-            if 'F' in line[5]:
+            elif 'L' in string[1]:
+                bla = bla
+            elif 'R' in string[1]:
+                bla = bla
+            elif 'FF' in string[1]:
                 type_w = 'f'
                 size = 4  # 2 слова х 2 байта(размер слова)
-                wib = info['word_in_block']
-                # info['value'] = byte_reader(type_w, size, wib)
-                number_of_line += 1
-                print(info, '\r\n')
-
+                offset = string[2]
+                name = string[0]
+                value = byte_reader(type_w, size, offset)
+                string.clear()
+                string.insert(0, name)
+                string.insert(1, value)
+    return struct
 
 
 if __name__ == "__main__":
-    parse_planner_rsf()
+    # Парсим текстовый файл planner.stn
+    struct = parse_planner_stn()
+    for s in struct: print(s)
+    print(10 * '\r\n')
+
+    # Парсим бинарный файл planner.rsf
+    struct_with_values = parse_planner_rsf(struct)
+    for s in struct_with_values: print(s)
+
+
+
