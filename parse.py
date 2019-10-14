a95b1cc95ce8ddd7919921a359728c4c76d4d533
k1r3al <shklyara94@gmail.com>
2019-10-15 00:59:16 +0300

    home rework

--- parse.py
+++ parse.py
@@ -19,12 +19,35 @@ def value_of_lines():
         return int(len(lines))
 
 
-def byte_reader(type_w, size, offset):
+def byte_reader(type_w, offset):
     with open(planner_RSF, 'rb') as file:
         header = file.read(14).decode('cp1252')  # Имя файла заголовка [0 - 14]
         offset *= 2  # Номер слова в блоке умножаем на размер слова (2 байта)
-        file.seek(
-            offset + 14)  # Прибавляем 14 байт, чтобы отсечь Имя файла заголовка
+        file.seek(offset + 14)  # Прибавляем 14 байт, чтобы отсечь Имя файла заголовка
+
+        if 'WW' in type_w:
+            type_w = 'I'  # UINT_2t
+            size = 2
+
+        elif 'SS' in type_w:
+            type_w = 'i'  # INT_2t
+            size = 2
+
+        elif 'UU' in type_w:
+            type_w = '<i'
+            size = 4  # Размер 4 байта потому что используется 2 слова х 2 байта идущие друг за другом
+
+        elif 'LL' in type_w:
+            type_w = '<i'
+            size = 4  # Размер 4 байта потому что используется 2 слова х 2 байта идущие друг за другом
+
+        elif 'RR' in type_w:
+            type_w = 'c'  # битовая переменная
+            size = 1  # Размер 1 байт
+
+        elif 'FF' in type_w:
+            type_w = 'f'
+            size = 4  # 2 слова х 2 байта(размер слова)
 
         word = file.read(size)
         value = unpack(type_w, word)[0]
@@ -86,14 +109,14 @@ def parse_planner_stn():
                 next_line = line_parser(number_of_line + 1)
                 prev_offset = int(line[1])
                 curr_offset = int(next_line[0])
-                ca = [line[0], line[5], [curr_offset, prev_offset]]
+                ca = [line[0], line[5], curr_offset]
                 struct.append(ca)
                 number_of_line += 2
             elif 'LL' in line[5]:
                 next_line = line_parser(number_of_line + 1)
                 prev_offset = int(line[1])
                 curr_offset = int(next_line[0])
-                ca = [line[0], line[5], [curr_offset, prev_offset]]
+                ca = [line[0], line[5],curr_offset]
                 struct.append(ca)
                 number_of_line += 2
             elif 'RR' in line[5]:
@@ -126,79 +149,32 @@ def parse_planner_rsf(struct, frame_size, frame):
     frame_rate = frame * frame_size
     for string in struct_with_values:
         if len(string) == 3:
-            if 'WW' in string[1]:
-                type_w = 'I'  # UINT_2t
-                size = 2
-                offset = string[2] + frame_rate
-                name = string[0]
-                value = byte_reader(type_w, size, offset)
-                string.clear()
-                string.insert(0, name)
-                string.insert(1, value)
-            elif 'SS' in string[1]:
-                type_w = 'i'  # INT_2t
-                size = 2
-                offset = string[2] + frame_rate
-                name = string[0]
-                value = byte_reader(type_w, size, offset)
-                string.clear()
-                string.insert(0, name)
-                string.insert(1, value)
-            elif 'UU' in string[1]:
-                type_w = '<i'
-                size = 4  # Размер 4 байта потому что используется 2 слова х 2 байта идущие друг за другом
-                offset = string[2][0] + frame_rate
-                name = string[0]
-                value = byte_reader(type_w, size, offset)
-                string.clear()
-                string.insert(0, name)
-                string.insert(1, value)
-            elif 'LL' in string[1]:
-                type_w = '<i'
-                size = 4  # Размер 4 байта потому что используется 2 слова х 2 байта идущие друг за другом
-                offset = string[2][0] + frame_rate
-                name = string[0]
-                value = byte_reader(type_w, size, offset)
-                string.clear()
-                string.insert(0, name)
-                string.insert(1, value)
-            elif 'RR' in string[1]:
-                type_w = 'c'  # битовая переменная
-                size = 1  # Размер 1 байт
-                offset = string[2] + frame_rate
-                name = string[0]
-                value = byte_reader(type_w, size, offset)
-                string.clear()
-                string.insert(0, name)
-                string.insert(1, value)
-            elif 'FF' in string[1]:
-                type_w = 'f'
-                size = 4  # 2 слова х 2 байта(размер слова)
-                offset = string[2] + frame_rate
-                name = string[0]
-                value = byte_reader(type_w, size, offset)
-                string.clear()
-                string.insert(0, name)
-                string.insert(1, value)
+            name = string[0]
+            type_w = string[1]
+            offset = string[2] + frame_rate
+
+            value = byte_reader(type_w, offset)
+            string.clear()
+            string.insert(0, name)
+            string.insert(1, value)
+
+
     return struct_with_values
 
 
 if __name__ == "__main__":
     # 1) Парсим текстовый файл
     struct = parse_planner_stn()
-    # for s in struct: print(s)
-    # print(20 * '\r\n')
-
-    # 2) Парсим один кадр
-    # struct_with_values = parse_planner_rsf(struct)
-    # for s in struct_with_values: print(s)
-
-    # 3) Парсим весь бинарник по кадрам
+    # 2) Вычисляем количество кадров
     frame_c = frame_counter(struct[1])
+    # 3) Парсим бинарник по кадрам
     for frame in range(frame_c):
         print('FRAME № %s\r\n' % frame)
+
         struct_with_values = parse_planner_rsf(struct[0], frame_c, frame)
-        if frame == 2:
-            print()
+
         for s in struct_with_values: print(s)
-        print(50 * '\r\n')
+
+        for i in range(50):
+            print(250 * '*')
+
