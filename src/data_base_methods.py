1f609ee0c95e62cbf4e132b1e62b8c60661c936c
Kirill Shklyarenko <shklyara94@gmail.com>
2019-11-09 18:24:47 +0300

    home v2.0

--- src/data_base_methods.py
+++ src/data_base_methods.py
@@ -23,7 +23,7 @@ def insert_data_to_db(table_name: str, cur: any, data: list):
         print(f'INSERT INTO "{table_name}" {data}')
 
 
-def read_data_from_db(table_name: str, cur: any, data: dict) -> any:
+def read_from_db(table_name: str, cur: any, data: dict) -> any:
     # Для того чтобы узнать имена полей таблицы
     cur.execute(f'SELECT * FROM "{table_name}";')
     col_names = []
@@ -35,7 +35,6 @@ def read_data_from_db(table_name: str, cur: any, data: dict) -> any:
     param_placeholders = ','.join(['%s' for x in range(len(data))])
     query = f'SELECT * FROM "{table_name}" WHERE ({columns}) = ({param_placeholders})'
     param_values = tuple(x for x in data.values())
-    # param_values = (3, 1)
     try:
         cur.execute(query, param_values)
     except Exception as e:
@@ -51,6 +50,7 @@ def read_data_from_db(table_name: str, cur: any, data: dict) -> any:
 
 
 def map_values(data: list, col_names: list) -> list:
+    z = []
     if type(data) is list:
         for group in data:
             for k, v in group.items():
@@ -82,6 +82,16 @@ def map_values(data: list, col_names: list) -> list:
                 elif 'velocity' in k:
                     group['numVelocityZone'] = group.pop('velocity')
                     break
+                elif 'possiblePeriod[' in k:
+                    z.append(v)
+                    group.clear()
+                    if len(z) == 6:
+                        data.append({'possiblePeriods': z})
+                        # z.clear()
+                    break
+                elif 'scanPeriodSeconds' in k:
+                    group['scanPeriod'] = group.pop('scanPeriodSeconds')
+                    break
                 elif 'nextUpdateTimeSeconds' in k:
                     group['nextTimeUpdate'] = group.pop('nextUpdateTimeSeconds')
                     break
@@ -101,6 +111,7 @@ def prepare_data_for_db(table_name: str, cur: any, data) -> list:
     data = map_values(data, col_names)
 
     if type(data) is dict:
+        # 2839
         for key, value in data.items():
             if key in col_names:
                 data_to_insert.append([key, value])
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from text_file_parser import *
 from bin_file_parser import *
 from data_base_methods import *
 import re
+import time
 
 
 if __name__ == "__main__":
@@ -12,9 +13,10 @@ if __name__ == "__main__":
     # Соединяемся с БД
     cur, conn = connection()
     # Парсим бинарник по кадрам
-    # frame_number = 1947
-    for frame_number in range(1063, frame_c):
-        print('\r\nFRAME № %s \r\n' % frame_number)
+    # frame_number = 8892
+    for frame_number in range(959, frame_c):
+        start_time = time.time()
+        print('\r\n\r\n\r\n\r\n         FRAME № %s \r\n' % frame_number)
         data = parse_bin_file(data_structure, frame_size, frame_number)
 
         primary_marks_count = 0
@@ -31,8 +33,12 @@ if __name__ == "__main__":
                 group.pop(0)
                 group.append(task_task_id)
                 z = prepare_data_for_db('BeamTasks', cur, group)
-                group.clear()
-                insert_data_to_db('BeamTasks', cur, z)
+                # group.clear()
+                z = dict(z)
+                bt_data = read_from_db('BeamTasks', cur, {f: z[f] for f in ('taskId', 'antennaId')})
+                if bt_data is None:
+                    z = [[k, v] for k, v in z.items()]
+                    insert_data_to_db('BeamTasks', cur, z)
 
             # ---------------------ЗАПОЛНЯЕМ "PrimaryMarks"----------------------#
             elif re.search(r'scanData', group[0]):
@@ -40,26 +46,35 @@ if __name__ == "__main__":
                 scandata = {}
                 for c in group:
                     scandata.update(c)
-                group.clear()
+                # group.clear()
 
             elif re.search(r'primaryMark', group[0]) and scandata['primaryMarksCount'] != 0:
                 primary_marks_count += 1
                 group.pop(0)
                 if primary_marks_count <= scandata['primaryMarksCount']:
-                    read_data = read_data_from_db('BeamTasks', cur,
-                                                  {k: scandata[k] for k in ('taskId', 'antennaId', 'taskType')})
-                    group.append({k: v for k, v in read_data.items() if k == 'BeamTask'})
+                    bt_data = read_from_db('BeamTasks', cur,
+                                           {k: scandata[k] for k in ('taskId',
+                                                                     'antennaId',
+                                                                     # 'taskType'
+                                                                     )})
+
+                    group.append({k: v for k, v in bt_data.items() if k == 'BeamTask'})
                     for k in [{k: v} for k, v in scandata.items()]:
                         group.append(k)
+
                     z = prepare_data_for_db('PrimaryMarks', cur, group)
-                    group.clear()
-                    insert_data_to_db('PrimaryMarks', cur, z)
+                    # group.clear()
+                    z = dict(z)
+                    pm_data = read_from_db('PrimaryMarks', cur, {k: v for k, v in z.items() if k == 'BeamTask'})
+
+                    if pm_data is None:
+                        z = [[k, v] for k, v in z.items()]
+                        insert_data_to_db('PrimaryMarks', cur, z)
 
             # ---------------------ЗАПОЛНЯЕМ "Candidates"----------------------#
             elif re.search(r'TrackCandidates', group[0]):
                 candidate_queue = {}
                 candidate_queue.update(group[1].items())
-                group.pop(0)
 
             elif re.search(r'trackCandidate', group[0]) and \
                     candidate_queue['candidatesQueueSize'] != 0:
@@ -68,22 +83,15 @@ if __name__ == "__main__":
                 track_candidate = {}
                 for c in group:
                     track_candidate.update(c)
-                group.clear()
+                # group.clear()
 
             elif re.search(r'viewSpot', group[0]) and \
                     candidate_queue['candidatesQueueSize'] != 0:
                 group.pop(0)
                 view_spot = {}
                 for c in group:
-                    if 'distancePeriod' in c:
-                        view_spot.update(c)
-                    elif 'velocityPeriod' in c:
-                        view_spot.update(c)
-                    elif 'distance' in c:
-                        view_spot.update(c)
-                    elif 'velocity' in c:
-                        view_spot.update(c)
-                group.clear()
+                    view_spot.update(c)
+                # group.clear()
 
             elif re.search(r'velocityResolutionSpot', group[0]) and \
                     candidate_queue['candidatesQueueSize'] != 0:
@@ -99,37 +107,32 @@ if __name__ == "__main__":
                     candidate.update(view_spot)
                     candidate.update(velocity_res_spot)
 
-                    bt_data = read_data_from_db('BeamTasks', cur, {k: candidate[k] for k in ('taskId', 'antennaId')})
+                    bt_data = read_from_db('BeamTasks', cur, {k: candidate[k] for k in ('taskId', 'antennaId')})
                     beam_task_pk = {k: v for k, v in bt_data.items() if k == 'BeamTask'}
 
                     cand_keys = {k: candidate[k] for k in ('azimuth', 'elevation', 'beamAzimuth', 'beamElevation')}
                     z = {}
                     z.update(beam_task_pk)
                     z.update(cand_keys)
-                    pm_data = read_data_from_db('PrimaryMarks', cur, z)
+                    pm_data = read_from_db('PrimaryMarks', cur, z)
 
                     if bt_data and pm_data:
                         primary_mark_pk = {k: v for k, v in pm_data.items() if k == 'PrimaryMark'}
                         candidate.update(beam_task_pk)
                         candidate.update(primary_mark_pk)
                         z = prepare_data_for_db('Candidates', cur, [{k: v} for k, v in candidate.items()])
-                        group.clear()
+                        # group.clear()
                         insert_data_to_db('Candidates', cur, z)
 
                 # ---------------------ЗАПОЛНЯЕМ "CandidatesIds"----------------------#
                 candidate_ids = {}
                 zapros = [
-                    'PrimaryMark',
-                    'BeamTask',
-                    # 'azimuth',
-                    # 'elevation'
+                    "azimuth", "elevation", "state", "numDistanceZone", "numVelocityZone",
+                    "distanceZoneWeight", "velocityZoneWeight", "nextTimeUpdate", "PrimaryMark", "BeamTask"
                 ]
                 # формирование строки запроса
                 columns = ','.join([f'"{x}"' for x in zapros])
-                # param_placeholders = ','.join(['%s' for x in range(len(data))])
                 query = f'SELECT DISTINCT ({columns}) FROM "Candidates"'
-                # param_values = tuple(x for x in data.values())
-                # param_values = (3, 1)
                 # try:
                 #     cur.execute(query)
                 # except Exception as e:
@@ -137,9 +140,10 @@ if __name__ == "__main__":
                 # else:
                 #     db_values = cur.fetchall()
                 #     if db_values:
-                #         for c, i in enumerate(db_values):
-                #             print(zapros)
-                #             print(db_values[c])
+                #         s = db_values[0][0]
+                #         ss = s.split(',')
+                #         print(ss)
+
                 # ---------------------ЗДЕСЬ------------------------------------------#
                 # ---------------------ДОЛЖНА БЫТЬ------------------------------------#
                 # ---------------------ВАША РЕКЛАМА-----------------------------------#
@@ -148,12 +152,6 @@ if __name__ == "__main__":
             elif re.search(r'\bTracks\b', group[0]):
                 tracks_queue = {}
                 tracks_queue.update(group[1].items())
-                group.pop(0)
-
-                # tracks_queue['tracksQueuesSize'] = 4
-
-                # if tracks_queue['tracksQueuesSize'] != 0:
-                # breakpoint()
 
             elif re.search(r'track_', group[0]) and tracks_queue['tracksQueuesSize'] != 0:
                 tracks_count += 1
@@ -161,22 +159,20 @@ if __name__ == "__main__":
                 track = {}
                 for c in group:
                     track.update(c)
-                group.clear()
+                # group.clear()
 
-                if track['antennaId'] != 0:
-                    breakpoint()
-
-                pm_data = read_data_from_db('PrimaryMarks', cur, {k: v for k, v in track.items() if k == 'antennaId'})
-                candidate_data = read_data_from_db('Candidates', cur, {k: track[k] for k in ('azimuth',
-                                                                                             'elevation')})
+                pm_data = read_from_db('PrimaryMarks', cur, {k: v for k, v in track.items() if k == 'antennaId'})
+                candidate_data = read_from_db('Candidates', cur, {k: track[k] for k in ('azimuth',
+                                                                                        'elevation')})
                 if pm_data and candidate_data:
                     pm_data_pk = {k: v for k, v in pm_data.items() if k == 'PrimaryMark'}
                     candidate_data_pk = {k: v for k, v in candidate_data.items() if k == 'Candidate'}
 
                     track.update(pm_data_pk)
                     track.update(candidate_data_pk)
-                    z = prepare_data_for_db('AirTracks', cur, track)
-                    group.clear()
+
+                    z = prepare_data_for_db('AirTracks', cur, [{k: v} for k, v in track.items()])
+                    # group.clear()
                     insert_data_to_db('AirTracks', cur, z)
 
             # ---------------------ЗАПОЛНЯЕМ "ForbiddenSectors"----------------------#
@@ -185,20 +181,23 @@ if __name__ == "__main__":
                 rad_forbidden_sector = {}
                 for c in group:
                     rad_forbidden_sector.update(c)
-                group.clear()
+                # group.clear()
 
                 if rad_forbidden_sector['RadiationForbiddenSectorsCount'] != 0:
                     breakpoint()
 
-            elif re.search(r'RadiationForbiddenSector', group[0]) and\
+            elif re.search(r'RadiationForbiddenSector', group[0]) and \
                     rad_forbidden_sector['RadiationForbiddenSectorsCount'] != 0:
                 rad_forbidden_count += 1
-                group.pop(0)
+
                 if rad_forbidden_count <= rad_forbidden_sector['RadiationForbiddenSectorsCount']:
                     for c in group:
                         rad_forbidden_sector.update(c)
 
                     z = prepare_data_for_db('ForbiddenSectors', cur, rad_forbidden_sector)
-                    group.clear()
-                    breakpoint()
+                    # group.clear()
+                    # breakpoint()
                     insert_data_to_db('ForbiddenSectors', cur, z)
+
+        time_sec = "{:7.4f}".format(time.time() - start_time)
+        print(f"----- {time_sec} seconds  -----")
