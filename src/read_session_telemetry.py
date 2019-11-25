160e90e0c73a0daa9a946487812f6b0c62f21ac7
sanchez.rick <sanchez.rick@astrosoft.ru>
2019-11-25 13:12:59 +0300

    small reworks in (class BinFrameReader)

--- src/read_session_telemetry.py
+++ src/read_session_telemetry.py
@@ -8,11 +8,10 @@ logging.config.dictConfig(DEFAULT_LOGGING)
 
 
 class BinFrameReader:
-    __slots__ = ('__file_obj', '__frame_rate', '__frame_size', '__header_size', '__frame_buffer')
+    __slots__ = ('__file_obj', '__frame_size', '__header_size', '__frame_buffer')
 
-    def __init__(self, file_name: str, frame_rate: int, frame_size: int, header_size=14):
+    def __init__(self, file_name: str, frame_size: int, header_size=14):
         self.__file_obj = open(file_name, 'rb', 1)
-        self.__frame_rate = frame_rate
         self.__frame_size = frame_size
         self.__header_size = header_size
         self.__frame_buffer = None
@@ -20,32 +19,31 @@ class BinFrameReader:
     def header_seeker(self) -> bytes:
         return self.__frame_buffer[self.__header_size:]
 
-    def init_to_start(self):
+    def init_to_start(self, frame_index):
         # обрезаем от конца(10) по сайзу -> # до 5 получаем фрейм № 2
-        # return self.__frame_buffer[self.__frame_rate:]
-        return self.__file_obj.seek(self.__frame_rate)
+        frame_rate = self.__frame_size * frame_index
+        return self.__file_obj.seek(frame_rate)
 
-    def read_next_frame(self) -> bytes:
+    def read_next_frame(self, frame_index) -> bytes:
         # например сайз=1, # текущий фрейм № 0 -> читаем до конца 1 * 0 + 1 = 1 -> фрейм рейт (может быть = 0)
-        self.__frame_buffer = self.init_to_start()
-        self.__frame_buffer = self.__file_obj.read(self.__frame_rate + self.__frame_size)
+        self.__frame_buffer = self.init_to_start(frame_index)
+        self.__frame_buffer = self.__file_obj.read(self.__frame_size)
         self.__frame_buffer = self.header_seeker()
         return self.__frame_buffer
 
 
 class TelemetryFrameIterator(BinFrameReader):
-    __slots__ = ('__data_struct', '__frame_size_in_bytes', '__frame_index', '__frame_rate',
+    __slots__ = ('__data_struct', '__frame_size_in_bytes', '__frame_index',
                  '__frame_buffer', '__serialize_string', '__frames_count')
 
     def __init__(self, file_name: str, structure, frame_index=0):
         self.__data_struct = structure.structure
         self.__frame_size_in_bytes = structure.frame_size * 2  # need 16072 bytes
         self.__frame_index = frame_index
-        self.__frame_rate = self.__frame_size_in_bytes * self.__frame_index
         self.__frame_buffer = None
         self.__serialize_string = self.create_serialize_string()
         self.__frames_count = self.frame_counter(file_name)
-        super().__init__(file_name, self.__frame_rate, self.__frame_size_in_bytes)
+        super().__init__(file_name, self.__frame_size_in_bytes)
 
     def create_serialize_string(self) -> str:
         serialize_string = '='
@@ -105,7 +103,7 @@ class TelemetryFrameIterator(BinFrameReader):
         return self
 
     def __next__(self):
-        self.__frame_buffer = self.read_next_frame()
+        self.__frame_buffer = self.read_next_frame(self.__frame_index)
         try:
             log.info(f'----------------------- FRAME {self.__frame_index} ------------------')
             result = self.__fill_session_structure()
